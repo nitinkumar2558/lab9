79e6df279eea19d26fc561cd178063684c8ede86
nitinkumar2558 <42238704+nitinkumar2558@users.noreply.github.com>
2018-11-09 16:40:38 +0000

    Update q17strcat.cpp

--- q17strcat.cpp
+++ q17strcat.cpp
@@ -1,14 +1,15 @@
 
 #include<iostream>
 using namespace std;
-void strcat(char a[20],char b[5]){
-char *p,*q;
-   cout<<"first string :"<<endl;
-   cin>>a;
-   cout<<"second string :"<<endl;
-   cin>>b;
-   p=a;
-   q=b;
+
+		void strcat(char a[20],char b[5]){
+			char *p,*q;
+	   	cout<<"first string :"<<endl;
+   		cin>>a;
+  	 cout<<"second string :"<<endl;
+   	cin>>b;
+   	p=a;
+   	q=b;
    
   //using loop
     while(*p!='\0'){
@@ -17,30 +18,16 @@ char *p,*q;
        *p='\0';
        }
     
-    
-   
-    
-		while(*q!='\0'){
-		if(*p='\0'){
-		*p=*q;
-		cout<<p;}
+   while(*q!='\0'){	
+	if(*p='\0'){
+	*p=*q;
+	cout<<p;}
 		
 		//*p=*q;
 		//cout<<p;
-		*q='\0';
-		*p='\0';
-      }
-      
-      
-     
-    
-  /*cout<<"resulting string is :";
-   while(*p!=0){
-	   cout<<*p ;
-    p++;}*/
-    
-  
-}
+	*q='\0';
+	*p='\0';
+   }}
 int main(){ 
 	char a[20],b[5];
    strcat(a,b);
