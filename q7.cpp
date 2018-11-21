046b5de2dd18e7643b1d702c287c3ce3953eadc9
nitinkumar2558 <42238704+nitinkumar2558@users.noreply.github.com>
2018-11-21 15:44:29 +0000

    Update q7.cpp

--- q7.cpp
+++ q7.cpp
@@ -1,6 +1,9 @@
-//Write a function that returns a pointer to the maximum value of an array of double's.  If the array is empty, return NULL.
 
-
+//program for max value in double array ,function return pointer
+#include<iostream>
+using namespace std;
+//prototype of function
+double *max(double*,int );
 #include<iostream>
 using namespace std;
 //prototype of function
@@ -15,25 +18,26 @@ cout<<*p<<endl;
 
 
 }
-double* max(double*aarray,int x){
-	if(x == 0){
-	return 0;}
-	else{
-			double *b;
+			double* max(double*aarray,int x){
+			if(x == 0){ return 0;}//size is zero ,array is empty 
+				
+			else{
+			
+				
+				double *ptr = aarray;//pointer point 1st element
+				double *furthermore = aarray + 1;//pointer point 2nd element
+				
 				double max = *aarray;
-				double *ptr = aarray;
-	
-				b = aarray + 1;
 				for(int i= 0; i<x; i++){
-					if (*b > max){
-					max = *b;
-					ptr = b;}//max is first element compare with b ,1 more than that
+					if (*furthermore > max){//if greter new max is value in furthermore
+					max = *furthermore;
+					ptr = furthermore;}//max is first element compare with futhermore,it increasing 1 by 1
+					furthermore++;
+						}
+				return ptr;}//new pointer for return
+				}
 
-					b++;
-	}
-	return ptr;
-}
-}
+
 
 
 
