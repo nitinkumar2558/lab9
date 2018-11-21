b6e49970bde4e98a33f1fbbeb2fa09cf4e30a0cd
nitinkumar2558 <42238704+nitinkumar2558@users.noreply.github.com>
2018-11-21 15:53:20 +0000

    Update q9.cpp

--- q9.cpp
+++ q9.cpp
@@ -4,33 +4,26 @@
 #include<iostream>
 using namespace std;
 
-bool check(char *p,char c)
-{
-    while(*p!='\0')
-    {
-        if(*p==c)
-        {
-            return 1;
-        }
+bool truefalse(char *p,char c){
+   while(*p!='\0')
+    {   if(*p==c)  { return 1;}//it just find character present in array
         p++;
     }
-    return 0;
+    return 0;//nothing is equal to c
 }
 
 int main()
 {
-    char string[50],c;
+    char a[100],c;
     cout<<"Enter a  string: ";
-    cin>>string;
+    cin>>a;
     cout<<"Enter the character which you want to check: ";
     cin>>c;
-    char *p=string;
-    bool t=check(p,c);
-    if(t==1)
-    {
-        cout<<c<<" is present in "<<string<<endl;
-    }
-    else
-        cout<<c<<" is not present in "<<string<<endl;
+    char *p=a;
+    bool t=truefalse(a,c);
+    if(t==1)  {cout<<c<<" is present in "<<a<<endl;}
+
+    else {cout<<c<<" is not present in "<<a<<endl;}
+   
         return 0;
 }
