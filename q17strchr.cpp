38921a083e05853da546636276cf3dc0bb46e51f
nitinkumar2558 <42238704+nitinkumar2558@users.noreply.github.com>
2018-11-21 07:43:46 +0000

    Update q17strchr.cpp

--- q17strchr.cpp
+++ q17strchr.cpp
@@ -1,29 +1,22 @@
 
+//nitin
+//kumar
+
 #include<iostream>
 using namespace std;
 
-int strchr(char a[],char i)
-{
-	char *p;int n=0;
-	p=a;
-	while(*p!='\0')
-		
-	{
-		if(*p==i)
-		n++;
-		p++;
-	}
-	return n;
-}
+
 int main()
 {
-		char a[15],i;
-		int k;
-		cout<<"Enter the string please "<<endl;
+		char a[5],k;
+		int count;
+		cout<<"Enter number string  "<<endl;
 		cin>>a;
-		cout<<"Enter the character you want to look "<<endl;
-		cin>>i;
-		k=strchr(a,i);
-		cout<<"number of times the character repeated in the string = "<<k;
-		return 0;
+		cout<<"Enter the character you want  "<<endl;
+		cin>>k;
+		
+for(int i=0;i<5;i++){
+if (a[i]==k){count++;}
+}
+cout<<count<<endl;
 }
